af2e189e49884a1855be4e351bcdd70405b83e17
Binaya Sharma <binaya.sharma@stackguardian.io>
2023-08-03 00:18:33 +0545

    Single container code push

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 resource "azurerm_storage_container" "main" {
-  for_each = var.storage_containers
-
-  name                  = each.value.name
+  name                  = var.container_name
   storage_account_name  = var.storage_account
-  container_access_type = each.value.container_access_type
+  container_access_type = var.container_access_type
 }
--- variable.tf
+++ variable.tf
@@ -1,21 +1,14 @@
 variable "storage_account" {
-  description = "Storage account names."
+  description = "(Required) The name of the Storage Account where the Container should be created. Changing this forces a new resource to be created."
   type        = string
 }
-variable "storage_containers" {
-  description = "Storage Container names."
-  type = map(object({
-    name                  = string
-    container_access_type = string
-  }))
-  default = {
-    runner = {
-      name                  = "runner"
-      container_access_type = "private"
-    }
-    system = {
-      name                  = "system"
-      container_access_type = "private"
-    }
-  }
+
+variable "container_name" {
+    description = "(Required) The name of the Container which should be created within the Storage Account. Changing this forces a new resource to be created."
+    type = string
+}
+
+variable "container_access_type" {
+    description = "(Optional) The Access Level configured for this Container. Possible values are blob, container or private. Defaults to private."
+    default = "private"
 }
