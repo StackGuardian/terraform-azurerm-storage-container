4ac3e2c031e229cf9b97e8e30c0b415c2f46be53
Binaya Sharma <binaya.sharma@stackguardian.io>
2023-08-03 00:08:05 +0545

    Adding storage account name as variable

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "azurerm_storage_container" "main" {
   for_each = var.storage_containers
 
   name                  = each.value.name
-  storage_account_name  = azurerm_storage_account.main.name
+  storage_account_name  = var.storage_account
   container_access_type = each.value.container_access_type
 }
--- variable.tf
+++ variable.tf
@@ -1,4 +1,9 @@
+variable "storage_account" {
+  description = "Storage account names."
+  type        = string
+}
 variable "storage_containers" {
+  description = "Storage Container names."
   type = map(object({
     name                  = string
     container_access_type = string
